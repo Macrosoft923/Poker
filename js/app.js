405958e8d61163a73dd87a222c21b59079f333f0
Smiling-Albert <107292560+Macrosoft923@users.noreply.github.com>
2024-04-19 11:40:30 +0900

    スタイルとカードの表示を変更

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // トランプデータの作成
 let playing_cards = [
+    { src: 'playing_cards/Playing_card_club_A.jpg', msg: 'A♧' },
     { src: 'playing_cards/Playing_card_club_2.jpg', msg: '2♧' },
     { src: 'playing_cards/Playing_card_club_3.jpg', msg: '3♧' },
     { src: 'playing_cards/Playing_card_club_4.jpg', msg: '4♧' },
@@ -9,37 +10,82 @@ let playing_cards = [
     { src: 'playing_cards/Playing_card_club_8.jpg', msg: '8♧' },
     { src: 'playing_cards/Playing_card_club_9.jpg', msg: '9♧' },
     { src: 'playing_cards/Playing_card_club_10.jpg', msg: '10♧' },
-    { src: 'playing_cards/Playing_card_club_A.jpg', msg: 'A♧' },
     { src: 'playing_cards/Playing_card_club_J.jpg', msg: 'J♧' },
     { src: 'playing_cards/Playing_card_club_K.jpg', msg: 'K♧' },
-    { src: 'playing_cards/Playing_card_club_Q.jpg', msg: 'Q♧' }
+    { src: 'playing_cards/Playing_card_club_Q.jpg', msg: 'Q♧' },
+    { src: 'playing_cards/Playing_card_diamond_A.jpg', msg: 'A♢' },
+    { src: 'playing_cards/Playing_card_diamond_2.jpg', msg: '2♢' },
+    { src: 'playing_cards/Playing_card_diamond_3.jpg', msg: '3♢' },
+    { src: 'playing_cards/Playing_card_diamond_4.jpg', msg: '4♢' },
+    { src: 'playing_cards/Playing_card_diamond_5.jpg', msg: '5♢' },
+    { src: 'playing_cards/Playing_card_diamond_6.jpg', msg: '6♢' },
+    { src: 'playing_cards/Playing_card_diamond_7.jpg', msg: '7♢' },
+    { src: 'playing_cards/Playing_card_diamond_8.jpg', msg: '8♢' },
+    { src: 'playing_cards/Playing_card_diamond_9.jpg', msg: '9♢' },
+    { src: 'playing_cards/Playing_card_diamond_10.jpg', msg: '10♢' },
+    { src: 'playing_cards/Playing_card_diamond_J.jpg', msg: 'J♢' },
+    { src: 'playing_cards/Playing_card_diamond_K.jpg', msg: 'K♢' },
+    { src: 'playing_cards/Playing_card_diamond_Q.jpg', msg: 'Q♢' },
+    { src: 'playing_cards/Playing_card_heart_A.jpg', msg: 'A♡' },
+    { src: 'playing_cards/Playing_card_heart_2.jpg', msg: '2♡' },
+    { src: 'playing_cards/Playing_card_heart_3.jpg', msg: '3♡' },
+    { src: 'playing_cards/Playing_card_heart_4.jpg', msg: '4♡' },
+    { src: 'playing_cards/Playing_card_heart_5.jpg', msg: '5♡' },
+    { src: 'playing_cards/Playing_card_heart_6.jpg', msg: '6♡' },
+    { src: 'playing_cards/Playing_card_heart_7.jpg', msg: '7♡' },
+    { src: 'playing_cards/Playing_card_heart_8.jpg', msg: '8♡' },
+    { src: 'playing_cards/Playing_card_heart_9.jpg', msg: '9♡' },
+    { src: 'playing_cards/Playing_card_heart_10.jpg', msg: '10♡' },
+    { src: 'playing_cards/Playing_card_heart_J.jpg', msg: 'J♡' },
+    { src: 'playing_cards/Playing_card_heart_K.jpg', msg: 'K♡' },
+    { src: 'playing_cards/Playing_card_heart_Q.jpg', msg: 'Q♡' },
+    { src: 'playing_cards/Playing_card_spade_A.jpg', msg: 'A♤' },
+    { src: 'playing_cards/Playing_card_spade_2.jpg', msg: '2♤' },
+    { src: 'playing_cards/Playing_card_spade_3.jpg', msg: '3♤' },
+    { src: 'playing_cards/Playing_card_spade_4.jpg', msg: '4♤' },
+    { src: 'playing_cards/Playing_card_spade_5.jpg', msg: '5♤' },
+    { src: 'playing_cards/Playing_card_spade_6.jpg', msg: '6♤' },
+    { src: 'playing_cards/Playing_card_spade_7.jpg', msg: '7♤' },
+    { src: 'playing_cards/Playing_card_spade_8.jpg', msg: '8♤' },
+    { src: 'playing_cards/Playing_card_spade_9.jpg', msg: '9♤' },
+    { src: 'playing_cards/Playing_card_spade_10.jpg', msg: '10♤' },
+    { src: 'playing_cards/Playing_card_spade_J.jpg', msg: 'J♤' },
+    { src: 'playing_cards/Playing_card_spade_K.jpg', msg: 'K♤' },
+    { src: 'playing_cards/Playing_card_spade_Q.jpg', msg: 'Q♤' }
 ];
 
 // 最初のデータを表示しておく
-let mainImage = document.createElement('img');
-mainImage.setAttribute('src', playing_cards[0].src);
-mainImage.setAttribute('alt', playing_cards[0].msg);
+let mainFlame = document.querySelector('#cards');
+let cardList = document.createElement('ul');
+for (let i = 0; i < 5; i++) {
+    let mainCard = document.createElement('img');
+    let index = Math.floor(Math.random() * 52);
+    mainCard.setAttribute('src', playing_cards[index].src);
+    mainCard.setAttribute('alt', playing_cards[index].msg);
 
-let mainMsg = document.createElement('p');
-mainMsg.innerText = mainImage.alt;
+    let mainMsg = document.createElement('p');
+    mainMsg.innerText = mainCard.alt;
 
-let mainFlame = document.querySelector('#gallery .main');
-mainFlame.insertBefore(mainImage, null);
-mainFlame.insertBefore(mainMsg, null);
-
-// サムネイル画像の表示
-let thumbFlame = document.querySelector('#gallery .thumb');
-for (let i = 0; i < playing_cards.length; i++) {
-    let thumbImage = document.createElement('img');
-    thumbImage.setAttribute('src', playing_cards[i].src);
-    thumbImage.setAttribute('alt', playing_cards[i].msg);
-    thumbFlame.insertBefore(thumbImage, null);
+    let cardItem = document.createElement('li');
+    cardItem.appendChild(mainCard);
+    cardItem.appendChild(mainMsg);
+    cardList.appendChild(cardItem);
 }
+mainFlame.appendChild(cardList);
 
-// クリックした画像をメインにする
-thumbFlame.addEventListener('click', function (event) {
-    if (event.target.src) {
-        mainImage.src = event.target.src;
-        mainMsg.innerText = event.target.alt;
-    }
-});
+// // サムネイル画像の表示
+// let thumbFlame = document.querySelector('#gallery .thumb');
+// for (let i = 0; i < playing_cards.length; i++) {
+//     let thumbImage = document.createElement('img');
+//     thumbImage.setAttribute('src', playing_cards[i].src);
+//     thumbImage.setAttribute('alt', playing_cards[i].msg);
+//     thumbFlame.insertBefore(thumbImage, null);
+// }
+
+// // クリックした画像をメインにする
+// thumbFlame.addEventListener('click', function (event) {
+//     if (event.target.src) {
+//         mainImage.src = event.target.src;
+//         mainMsg.innerText = event.target.alt;
+//     }
+// });
