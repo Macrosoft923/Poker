46a31b5168d77b0ed0dd9182513b0a929c57291d
Smiling-Albert <107292560+Macrosoft923@users.noreply.github.com>
2024-04-20 03:28:55 +0900

    トランプカードの背景色を緑に変更しました

--- js/app.js
+++ js/app.js
@@ -1,57 +1,65 @@
 // トランプカードの作成
 let playing_cards = [
-    [{ src: 'playing_cards/Playing_card_club_A.jpg', msg: 'A♧' },
-    { src: 'playing_cards/Playing_card_club_2.jpg', msg: '2♧' },
-    { src: 'playing_cards/Playing_card_club_3.jpg', msg: '3♧' },
-    { src: 'playing_cards/Playing_card_club_4.jpg', msg: '4♧' },
-    { src: 'playing_cards/Playing_card_club_5.jpg', msg: '5♧' },
-    { src: 'playing_cards/Playing_card_club_6.jpg', msg: '6♧' },
-    { src: 'playing_cards/Playing_card_club_7.jpg', msg: '7♧' },
-    { src: 'playing_cards/Playing_card_club_8.jpg', msg: '8♧' },
-    { src: 'playing_cards/Playing_card_club_9.jpg', msg: '9♧' },
-    { src: 'playing_cards/Playing_card_club_10.jpg', msg: '10♧' },
-    { src: 'playing_cards/Playing_card_club_J.jpg', msg: 'J♧' },
-    { src: 'playing_cards/Playing_card_club_Q.jpg', msg: 'Q♧' },
-    { src: 'playing_cards/Playing_card_club_K.jpg', msg: 'K♧' }],
-    [{ src: 'playing_cards/Playing_card_diamond_A.jpg', msg: 'A♢' },
-    { src: 'playing_cards/Playing_card_diamond_2.jpg', msg: '2♢' },
-    { src: 'playing_cards/Playing_card_diamond_3.jpg', msg: '3♢' },
-    { src: 'playing_cards/Playing_card_diamond_4.jpg', msg: '4♢' },
-    { src: 'playing_cards/Playing_card_diamond_5.jpg', msg: '5♢' },
-    { src: 'playing_cards/Playing_card_diamond_6.jpg', msg: '6♢' },
-    { src: 'playing_cards/Playing_card_diamond_7.jpg', msg: '7♢' },
-    { src: 'playing_cards/Playing_card_diamond_8.jpg', msg: '8♢' },
-    { src: 'playing_cards/Playing_card_diamond_9.jpg', msg: '9♢' },
-    { src: 'playing_cards/Playing_card_diamond_10.jpg', msg: '10♢' },
-    { src: 'playing_cards/Playing_card_diamond_J.jpg', msg: 'J♢' },
-    { src: 'playing_cards/Playing_card_diamond_Q.jpg', msg: 'Q♢' },
-    { src: 'playing_cards/Playing_card_diamond_K.jpg', msg: 'K♢' }],
-    [{ src: 'playing_cards/Playing_card_heart_A.jpg', msg: 'A♡' },
-    { src: 'playing_cards/Playing_card_heart_2.jpg', msg: '2♡' },
-    { src: 'playing_cards/Playing_card_heart_3.jpg', msg: '3♡' },
-    { src: 'playing_cards/Playing_card_heart_4.jpg', msg: '4♡' },
-    { src: 'playing_cards/Playing_card_heart_5.jpg', msg: '5♡' },
-    { src: 'playing_cards/Playing_card_heart_6.jpg', msg: '6♡' },
-    { src: 'playing_cards/Playing_card_heart_7.jpg', msg: '7♡' },
-    { src: 'playing_cards/Playing_card_heart_8.jpg', msg: '8♡' },
-    { src: 'playing_cards/Playing_card_heart_9.jpg', msg: '9♡' },
-    { src: 'playing_cards/Playing_card_heart_10.jpg', msg: '10♡' },
-    { src: 'playing_cards/Playing_card_heart_J.jpg', msg: 'J♡' },
-    { src: 'playing_cards/Playing_card_heart_Q.jpg', msg: 'Q♡' },
-    { src: 'playing_cards/Playing_card_heart_K.jpg', msg: 'K♡' }],
-    [{ src: 'playing_cards/Playing_card_spade_A.jpg', msg: 'A♤' },
-    { src: 'playing_cards/Playing_card_spade_2.jpg', msg: '2♤' },
-    { src: 'playing_cards/Playing_card_spade_3.jpg', msg: '3♤' },
-    { src: 'playing_cards/Playing_card_spade_4.jpg', msg: '4♤' },
-    { src: 'playing_cards/Playing_card_spade_5.jpg', msg: '5♤' },
-    { src: 'playing_cards/Playing_card_spade_6.jpg', msg: '6♤' },
-    { src: 'playing_cards/Playing_card_spade_7.jpg', msg: '7♤' },
-    { src: 'playing_cards/Playing_card_spade_8.jpg', msg: '8♤' },
-    { src: 'playing_cards/Playing_card_spade_9.jpg', msg: '9♤' },
-    { src: 'playing_cards/Playing_card_spade_10.jpg', msg: '10♤' },
-    { src: 'playing_cards/Playing_card_spade_J.jpg', msg: 'J♤' },
-    { src: 'playing_cards/Playing_card_spade_Q.jpg', msg: 'Q♤' },
-    { src: 'playing_cards/Playing_card_spade_K.jpg', msg: 'K♤' }]
+    [
+        { src: 'playing_cards/Playing_card_club_A.jpg', msg: 'A♧' },
+        { src: 'playing_cards/Playing_card_club_2.jpg', msg: '2♧' },
+        { src: 'playing_cards/Playing_card_club_3.jpg', msg: '3♧' },
+        { src: 'playing_cards/Playing_card_club_4.jpg', msg: '4♧' },
+        { src: 'playing_cards/Playing_card_club_5.jpg', msg: '5♧' },
+        { src: 'playing_cards/Playing_card_club_6.jpg', msg: '6♧' },
+        { src: 'playing_cards/Playing_card_club_7.jpg', msg: '7♧' },
+        { src: 'playing_cards/Playing_card_club_8.jpg', msg: '8♧' },
+        { src: 'playing_cards/Playing_card_club_9.jpg', msg: '9♧' },
+        { src: 'playing_cards/Playing_card_club_10.jpg', msg: '10♧' },
+        { src: 'playing_cards/Playing_card_club_J.jpg', msg: 'J♧' },
+        { src: 'playing_cards/Playing_card_club_Q.jpg', msg: 'Q♧' },
+        { src: 'playing_cards/Playing_card_club_K.jpg', msg: 'K♧' }
+    ],
+    [
+        { src: 'playing_cards/Playing_card_diamond_A.jpg', msg: 'A♢' },
+        { src: 'playing_cards/Playing_card_diamond_2.jpg', msg: '2♢' },
+        { src: 'playing_cards/Playing_card_diamond_3.jpg', msg: '3♢' },
+        { src: 'playing_cards/Playing_card_diamond_4.jpg', msg: '4♢' },
+        { src: 'playing_cards/Playing_card_diamond_5.jpg', msg: '5♢' },
+        { src: 'playing_cards/Playing_card_diamond_6.jpg', msg: '6♢' },
+        { src: 'playing_cards/Playing_card_diamond_7.jpg', msg: '7♢' },
+        { src: 'playing_cards/Playing_card_diamond_8.jpg', msg: '8♢' },
+        { src: 'playing_cards/Playing_card_diamond_9.jpg', msg: '9♢' },
+        { src: 'playing_cards/Playing_card_diamond_10.jpg', msg: '10♢' },
+        { src: 'playing_cards/Playing_card_diamond_J.jpg', msg: 'J♢' },
+        { src: 'playing_cards/Playing_card_diamond_Q.jpg', msg: 'Q♢' },
+        { src: 'playing_cards/Playing_card_diamond_K.jpg', msg: 'K♢' }
+    ],
+    [
+        { src: 'playing_cards/Playing_card_heart_A.jpg', msg: 'A♡' },
+        { src: 'playing_cards/Playing_card_heart_2.jpg', msg: '2♡' },
+        { src: 'playing_cards/Playing_card_heart_3.jpg', msg: '3♡' },
+        { src: 'playing_cards/Playing_card_heart_4.jpg', msg: '4♡' },
+        { src: 'playing_cards/Playing_card_heart_5.jpg', msg: '5♡' },
+        { src: 'playing_cards/Playing_card_heart_6.jpg', msg: '6♡' },
+        { src: 'playing_cards/Playing_card_heart_7.jpg', msg: '7♡' },
+        { src: 'playing_cards/Playing_card_heart_8.jpg', msg: '8♡' },
+        { src: 'playing_cards/Playing_card_heart_9.jpg', msg: '9♡' },
+        { src: 'playing_cards/Playing_card_heart_10.jpg', msg: '10♡' },
+        { src: 'playing_cards/Playing_card_heart_J.jpg', msg: 'J♡' },
+        { src: 'playing_cards/Playing_card_heart_Q.jpg', msg: 'Q♡' },
+        { src: 'playing_cards/Playing_card_heart_K.jpg', msg: 'K♡' }
+    ],
+    [
+        { src: 'playing_cards/Playing_card_spade_A.jpg', msg: 'A♤' },
+        { src: 'playing_cards/Playing_card_spade_2.jpg', msg: '2♤' },
+        { src: 'playing_cards/Playing_card_spade_3.jpg', msg: '3♤' },
+        { src: 'playing_cards/Playing_card_spade_4.jpg', msg: '4♤' },
+        { src: 'playing_cards/Playing_card_spade_5.jpg', msg: '5♤' },
+        { src: 'playing_cards/Playing_card_spade_6.jpg', msg: '6♤' },
+        { src: 'playing_cards/Playing_card_spade_7.jpg', msg: '7♤' },
+        { src: 'playing_cards/Playing_card_spade_8.jpg', msg: '8♤' },
+        { src: 'playing_cards/Playing_card_spade_9.jpg', msg: '9♤' },
+        { src: 'playing_cards/Playing_card_spade_10.jpg', msg: '10♤' },
+        { src: 'playing_cards/Playing_card_spade_J.jpg', msg: 'J♤' },
+        { src: 'playing_cards/Playing_card_spade_Q.jpg', msg: 'Q♤' },
+        { src: 'playing_cards/Playing_card_spade_K.jpg', msg: 'K♤' }
+    ]
 ];
 
 // 最初のカードを表示しておく
@@ -100,20 +108,3 @@ setTimeout(() => {
         cardList.insertBefore(cardItem, null);
     }
 }, 3000);
-
-// // サムネイル画像の表示
-// let thumbFlame = document.querySelector('#gallery .thumb');
-// for (let i = 0; i < playing_cards.length; i++) {
-//     let thumbImage = document.createElement('img');
-//     thumbImage.setAttribute('src', playing_cards[i].src);
-//     thumbImage.setAttribute('alt', playing_cards[i].msg);
-//     thumbFlame.insertBefore(thumbImage, null);
-// }
-
-// // クリックした画像をメインにする
-// thumbFlame.addEventListener('click', function (event) {
-//     if (event.target.src) {
-//         mainImage.src = event.target.src;
-//         mainMsg.innerText = event.target.alt;
-//     }
-// });
