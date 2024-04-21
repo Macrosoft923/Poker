44f84aa23015ebcf4fca7240912789c891b083f4
Smiling-Albert <107292560+Macrosoft923@users.noreply.github.com>
2024-04-21 15:39:22 +0900

    コードの変更点を簡潔に説明するための新しいコミットメッセージ

--- js/app.js
+++ js/app.js
@@ -1,70 +1,74 @@
 /* トランプのカードの定義 ************************/
-// トランプのカードの番号を設定
+// トランプのカードの番号と絵柄を設定する配列
 let playing_cards = [
+    // クラブ
     [
-        { src: 'playing_cards/Playing_card_club_A.jpg', msg: 'A♧' },
-        { src: 'playing_cards/Playing_card_club_2.jpg', msg: '2♧' },
-        { src: 'playing_cards/Playing_card_club_3.jpg', msg: '3♧' },
-        { src: 'playing_cards/Playing_card_club_4.jpg', msg: '4♧' },
-        { src: 'playing_cards/Playing_card_club_5.jpg', msg: '5♧' },
-        { src: 'playing_cards/Playing_card_club_6.jpg', msg: '6♧' },
-        { src: 'playing_cards/Playing_card_club_7.jpg', msg: '7♧' },
-        { src: 'playing_cards/Playing_card_club_8.jpg', msg: '8♧' },
-        { src: 'playing_cards/Playing_card_club_9.jpg', msg: '9♧' },
-        { src: 'playing_cards/Playing_card_club_10.jpg', msg: '10♧' },
-        { src: 'playing_cards/Playing_card_club_J.jpg', msg: 'J♧' },
-        { src: 'playing_cards/Playing_card_club_Q.jpg', msg: 'Q♧' },
-        { src: 'playing_cards/Playing_card_club_K.jpg', msg: 'K♧' }
+        { src: 'playing_cards/Playing_card_club_A.jpg', msg: 'A♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_2.jpg', msg: '2♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_3.jpg', msg: '3♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_4.jpg', msg: '4♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_5.jpg', msg: '5♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_6.jpg', msg: '6♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_7.jpg', msg: '7♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_8.jpg', msg: '8♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_9.jpg', msg: '9♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_10.jpg', msg: '10♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_J.jpg', msg: 'J♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_Q.jpg', msg: 'Q♧', cnt: 1 },
+        { src: 'playing_cards/Playing_card_club_K.jpg', msg: 'K♧', cnt: 1 }
     ],
+    // ダイヤ
     [
-        { src: 'playing_cards/Playing_card_diamond_A.jpg', msg: 'A♢' },
-        { src: 'playing_cards/Playing_card_diamond_2.jpg', msg: '2♢' },
-        { src: 'playing_cards/Playing_card_diamond_3.jpg', msg: '3♢' },
-        { src: 'playing_cards/Playing_card_diamond_4.jpg', msg: '4♢' },
-        { src: 'playing_cards/Playing_card_diamond_5.jpg', msg: '5♢' },
-        { src: 'playing_cards/Playing_card_diamond_6.jpg', msg: '6♢' },
-        { src: 'playing_cards/Playing_card_diamond_7.jpg', msg: '7♢' },
-        { src: 'playing_cards/Playing_card_diamond_8.jpg', msg: '8♢' },
-        { src: 'playing_cards/Playing_card_diamond_9.jpg', msg: '9♢' },
-        { src: 'playing_cards/Playing_card_diamond_10.jpg', msg: '10♢' },
-        { src: 'playing_cards/Playing_card_diamond_J.jpg', msg: 'J♢' },
-        { src: 'playing_cards/Playing_card_diamond_Q.jpg', msg: 'Q♢' },
-        { src: 'playing_cards/Playing_card_diamond_K.jpg', msg: 'K♢' }
+        { src: 'playing_cards/Playing_card_diamond_A.jpg', msg: 'A♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_2.jpg', msg: '2♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_3.jpg', msg: '3♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_4.jpg', msg: '4♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_5.jpg', msg: '5♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_6.jpg', msg: '6♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_7.jpg', msg: '7♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_8.jpg', msg: '8♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_9.jpg', msg: '9♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_10.jpg', msg: '10♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_J.jpg', msg: 'J♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_Q.jpg', msg: 'Q♢', cnt: 1 },
+        { src: 'playing_cards/Playing_card_diamond_K.jpg', msg: 'K♢', cnt: 1 }
     ],
+    // ハート
     [
-        { src: 'playing_cards/Playing_card_heart_A.jpg', msg: 'A♡' },
-        { src: 'playing_cards/Playing_card_heart_2.jpg', msg: '2♡' },
-        { src: 'playing_cards/Playing_card_heart_3.jpg', msg: '3♡' },
-        { src: 'playing_cards/Playing_card_heart_4.jpg', msg: '4♡' },
-        { src: 'playing_cards/Playing_card_heart_5.jpg', msg: '5♡' },
-        { src: 'playing_cards/Playing_card_heart_6.jpg', msg: '6♡' },
-        { src: 'playing_cards/Playing_card_heart_7.jpg', msg: '7♡' },
-        { src: 'playing_cards/Playing_card_heart_8.jpg', msg: '8♡' },
-        { src: 'playing_cards/Playing_card_heart_9.jpg', msg: '9♡' },
-        { src: 'playing_cards/Playing_card_heart_10.jpg', msg: '10♡' },
-        { src: 'playing_cards/Playing_card_heart_J.jpg', msg: 'J♡' },
-        { src: 'playing_cards/Playing_card_heart_Q.jpg', msg: 'Q♡' },
-        { src: 'playing_cards/Playing_card_heart_K.jpg', msg: 'K♡' }
+        { src: 'playing_cards/Playing_card_heart_A.jpg', msg: 'A♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_2.jpg', msg: '2♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_3.jpg', msg: '3♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_4.jpg', msg: '4♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_5.jpg', msg: '5♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_6.jpg', msg: '6♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_7.jpg', msg: '7♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_8.jpg', msg: '8♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_9.jpg', msg: '9♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_10.jpg', msg: '10♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_J.jpg', msg: 'J♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_Q.jpg', msg: 'Q♡', cnt: 1 },
+        { src: 'playing_cards/Playing_card_heart_K.jpg', msg: 'K♡', cnt: 1 }
     ],
+    // スペード
     [
-        { src: 'playing_cards/Playing_card_spade_A.jpg', msg: 'A♤' },
-        { src: 'playing_cards/Playing_card_spade_2.jpg', msg: '2♤' },
-        { src: 'playing_cards/Playing_card_spade_3.jpg', msg: '3♤' },
-        { src: 'playing_cards/Playing_card_spade_4.jpg', msg: '4♤' },
-        { src: 'playing_cards/Playing_card_spade_5.jpg', msg: '5♤' },
-        { src: 'playing_cards/Playing_card_spade_6.jpg', msg: '6♤' },
-        { src: 'playing_cards/Playing_card_spade_7.jpg', msg: '7♤' },
-        { src: 'playing_cards/Playing_card_spade_8.jpg', msg: '8♤' },
-        { src: 'playing_cards/Playing_card_spade_9.jpg', msg: '9♤' },
-        { src: 'playing_cards/Playing_card_spade_10.jpg', msg: '10♤' },
-        { src: 'playing_cards/Playing_card_spade_J.jpg', msg: 'J♤' },
-        { src: 'playing_cards/Playing_card_spade_Q.jpg', msg: 'Q♤' },
-        { src: 'playing_cards/Playing_card_spade_K.jpg', msg: 'K♤' }
+        { src: 'playing_cards/Playing_card_spade_A.jpg', msg: 'A♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_2.jpg', msg: '2♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_3.jpg', msg: '3♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_4.jpg', msg: '4♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_5.jpg', msg: '5♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_6.jpg', msg: '6♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_7.jpg', msg: '7♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_8.jpg', msg: '8♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_9.jpg', msg: '9♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_10.jpg', msg: '10♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_J.jpg', msg: 'J♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_Q.jpg', msg: 'Q♤', cnt: 1 },
+        { src: 'playing_cards/Playing_card_spade_K.jpg', msg: 'K♤', cnt: 1 }
     ]
 ];
 
 /* プログラムで使用する変数の設定 ***************/
-// プログラムの要素を取得
+// プログラムの要素を取得する変数
 let button = document.getElementById('button');
 let cardsFlame = document.getElementById('cardsFlame');
 let unsortedCards = document.getElementById('unsorted');
@@ -73,75 +77,75 @@ let unsortedList = unsortedCards.querySelector('ul');
 let sortedList = sortedCards.querySelector('ul');
 
 /* イベント処理 ***************/
-// ポーカーを開始するボタンを押したとき
+// ポーカーを開始するボタンを押したときの処理
 button.addEventListener('click', function () {
-    // カードオブジェクトの定義
+    // カードオブジェクトを格納する配列
     let cards = [];
 
-    // ボタンを非表示
+    // ボタンを非表示にする
     button.style.display = 'none';
 
-    // 未ソートのカードを表示
+    // 未ソートのカードを表示する
     unsortedCards.style.display = 'block';
     unsortedList.style.display = 'block';
 
-    // cards = getTestCards();
-    cards = getCards();
+    // カードを取得し、未ソートのリストに表示する
+    // cards = getTestCards(playing_cards);
+    cards = getCards(playing_cards);
     displayCards(unsortedList, cards);
 
     // 1.0秒後にカードを並び替える
     setTimeout(() => {
-        // 未ソートのカードを非表示
+        // 未ソートのカードを非表示にする
         unsortedCards.style.display = 'none';
         unsortedList.style.display = 'none';
 
-        // ソート済みのカードを表示
+        // ソート済みのカードを表示する
         sortedCards.style.display = 'block';
         sortedList.style.display = 'block';
 
+        // カードを並び替え、ソート済みのリストに表示する
         sortCards(cards);
         displayCards(sortedList, cards);
 
-        // 0.5秒後に最終的な結果のメッセージ
+        // 0.5秒後に最終的な結果のメッセージを表示する
         setTimeout(() => {
-            // ソート済みのカードを非表示
-            // sortedCards.style.display = 'none';
-            // sortedList.style.display = 'none';
-
-            // 結果の判定
+            // 結果の判定を行い、アラートを表示する
             alert(getResultMsg(cards));
-
-            // ボタンを表示
-            // button.style.display = 'block';
         }, 500);
     }, 1000);
 });
 
 /* 関数定義 ************************/
-// テスト用のカードを取得する
-function getTestCards() {
+// テスト用のカードを取得する関数
+function getTestCards(playing_cards) {
     let cards = [];
-    let random = Math.floor(Math.random() * 9);
-    for (let i = 0; i < 5; i++) {
-        let row = Math.floor(Math.random() * 4);
-        let column = random + i;
-        cards.push({ suit: row, number: column });
+    while (cards.length < 5) {
+        let row = 0;
+        let column = Math.floor(Math.random() * 13);
+        if (playing_cards[row][column].cnt === 1) {
+            playing_cards[row][column].cnt--;
+            cards.push({ suit: row, number: column });
+        }
     }
     return cards;
 }
 
-// カードを取得する
-function getCards() {
+// カードを取得する関数
+function getCards(playing_cards) {
     let cards = [];
-    for (let i = 0; i < 5; i++) {
+    while (cards.length < 5) {
         let row = Math.floor(Math.random() * 4);
         let column = Math.floor(Math.random() * 13);
-        cards.push({ suit: row, number: column });
+        if (playing_cards[row][column].cnt === 1) {
+            playing_cards[row][column].cnt--;
+            cards.push({ suit: row, number: column });
+        }
     }
     return cards;
 }
 
-// カードを並び替える
+// カードを並び替える関数
 function sortCards(cards) {
     cards.sort((a, b) => {
         if (a.number === b.number) {
@@ -151,7 +155,7 @@ function sortCards(cards) {
     });
 }
 
-// カードを表示する
+// カードを表示する関数
 function displayCards(list, cards) {
     let items = list.querySelectorAll('li');
     for (let i = 0; i < 5; i++) {
@@ -162,26 +166,30 @@ function displayCards(list, cards) {
         let cardMsg = document.createElement('p');
         cardMsg.innerText = pokerHand.alt;
 
-        let cardItem = document.createElement('li');
         items[i].insertBefore(pokerHand, null);
         items[i].insertBefore(cardMsg, null);
     }
 }
 
-// 結果の判定
+// 結果の判定を行う関数
 function getResult(cards) {
+    // カードの絵柄と数字の値を取得する
     let suits = cards.map(card => card.suit);
     let values = cards.map(card => card.number);
 
+    // カード番号の出現回数を数えるためのMapオブジェクト
     let countMap = new Map();
     values.forEach(value => {
         let count = countMap.get(value) || 0;
         countMap.set(value, count + 1);
     });
 
+    // ペア、スリーカード、フォーカードの個数を初期化
     let countPair = 0;
     let countTrio = 0;
     let countQuartet = 0;
+
+    // 各カード番号の出現回数に応じて、ペア、スリーカード、フォーカードの個数をカウントする
     countMap.forEach(element => {
         if (element === 2) {
             countPair++;
@@ -192,6 +200,7 @@ function getResult(cards) {
         }
     });
 
+    // ストレートフラッシュを判定する関数
     function isStraightFlush(values, suits) {
         if (isStraight(values) && isFlush(suits)) {
             return true;
@@ -199,6 +208,7 @@ function getResult(cards) {
         return false;
     }
 
+    // フォー・オブ・ア・カインドを判定する関数
     function isQuads(countQuartet) {
         if (countQuartet === 1) {
             return true;
@@ -206,6 +216,7 @@ function getResult(cards) {
         return false;
     }
 
+    // フルハウスを判定する関数
     function isFullHouse(countPair, countTrio) {
         if (isOnePair(countPair) && isTrips(countTrio)) {
             return true;
@@ -213,6 +224,7 @@ function getResult(cards) {
         return false;
     }
 
+    // フラッシュを判定する関数
     function isFlush(suits) {
         for (let i = 1; i < 5; i++) {
             if (suits[i] !== suits[i - 1]) {
@@ -222,6 +234,7 @@ function getResult(cards) {
         return true;
     }
 
+    // ストレートを判定する関数
     function isStraight(values) {
         for (let i = 1; i < 4; i++) {
             if (values[i] !== values[i - 1] + 1) {
@@ -231,6 +244,7 @@ function getResult(cards) {
         return true;
     }
 
+    // スリー・オブ・ア・カインドを判定する関数
     function isTrips(countTrio) {
         if (countTrio === 1) {
             return true;
@@ -238,6 +252,7 @@ function getResult(cards) {
         return false;
     }
 
+    // ツーペアを判定する関数
     function isTwoPair(countPair) {
         if (countPair === 2) {
             return true;
@@ -245,6 +260,7 @@ function getResult(cards) {
         return false;
     }
 
+    // ワンペアを判定する関数
     function isOnePair(countPair) {
         if (countPair === 1) {
             return true;
@@ -252,6 +268,7 @@ function getResult(cards) {
         return false;
     }
 
+    // 役の強さに応じて結果を返す
     if (isStraightFlush(values, suits)) {
         return 'ストレートフラッシュ';
     } else if (isQuads(countQuartet)) {
@@ -273,7 +290,7 @@ function getResult(cards) {
     }
 }
 
-// 最終的な結果のメッセージ
+// 最終的な結果のメッセージを生成する関数
 function getResultMsg(cards) {
     return 'ポーカー・ハンドは「' + getResult(cards) + '」でした';
 }
